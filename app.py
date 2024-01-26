fe98695bf5dcf6372ac50de116dd03d35349ed43
Kate Moser <komoser@gmail.com>
2024-01-25 19:18:23 -0800

    added db connection, get routes for photos

--- app.py
+++ app.py
@@ -1,32 +1,82 @@
 """Application for uploading postcards"""
 
 import os
+from dotenv import load_dotenv
 
 from flask import Flask, jsonify, request
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from uuid import uuid4 as uuid
+
 import boto3
 
-app = Flask(__name__)
+from models import (
+    db,
+    connect_db,
+    Photo
+)
 
-s3 = boto3.client("s3")
+load_dotenv()
 
 BUCKET_NAME = os.environ["BUCKET_NAME"]
 
+app = Flask(__name__)
+
+# Configure app and db
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_ECHO'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+
+# toolbar = DebugToolbarExtension(app)
+s3 = boto3.client("s3")
+
+connect_db(app)
+
+
 @app.get("/api/photos")
 def get_all_photos():
-    """get info on all photos"""
+    """
+    Get info on all photos. Returns JSON like:
 
-    return jsonify("These are all the photos")
+    {
+        "photos": [
+            {
+                "created_at": "Fri, 26 Jan 2024 02:57:30 GMT",
+                "id": 1,
+                "image_url": "01ea49fa-b20e-462d-b5b5-e8b084f8ae5c"
+            }, {}...
+        ]
+    }
+    """
+
+    photos = Photo.query.all()
+
+    serialized = [p.serialize() for p in photos]
+
+    return jsonify(photos=serialized)
 
 
 @app.post("/api/photos")
 def upload_photo():
-    """upload a fileobj to s3 and create photo resource"""
+    """
+    Upload a fileobj to s3 and create photo resource. Returns JSON like:
 
-    new_photo = request.files['photo']
-    file_name = new_photo.filename
+    {
+        "photo": {
+            "created_at": "Fri, 26 Jan 2024 02:57:30 GMT",
+            "id": 1,
+            "image_url": "01ea49fa-b20e-462d-b5b5-e8b084f8ae5c"
+        }
+    }
+    """
+
+    file_obj = request.files['photo']
+    file_name = str(uuid())
 
     response = s3.upload_fileobj(
-        new_photo,
+        file_obj,
         BUCKET_NAME,
         file_name,
         ExtraArgs={
@@ -34,9 +84,34 @@ def upload_photo():
         }
     )
 
-    print("response:", response)
+    photo = Photo(
+        image_url=file_name
+    )
 
-    return jsonify({"response": "uploaded"})
+    print("response:", response, "photo:", photo)
+
+    db.session.add(photo)
+    db.session.commit()
+
+    return jsonify(photo.serialize())
+
+@app.get("/api/photos/<int:id>")
+def get_photo(id):
+    """
+    Get info on one photo. Returns JSON like:
+
+    {
+        "photo": {
+            "created_at": "Fri, 26 Jan 2024 02:57:30 GMT",
+            "id": 1,
+            "image_url": "01ea49fa-b20e-462d-b5b5-e8b084f8ae5c"
+        }
+    }
+    """
+
+    photo = Photo.query.get_or_404(id)
+    # TODO: More graceful handling of 404 -- make sure I'm sending back a JSON response.
+    return jsonify(photo=photo.serialize())
 
 
 @app.get("/api/postcards")
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """models for postcard applicaiton"""
 
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -10,5 +11,36 @@ class Photo(db.Model):
 
     id = db.Column(
         db.Integer,
-        primary_key=True
+        primary_key=True,
+        autoincrement=True,
     )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+    )
+
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+    )
+
+    def serialize(self):
+        """return json serializable dict of data"""
+
+        return{
+            "id": self.id,
+            "image_url": self.image_url,
+            "created_at": self.created_at,
+        }
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
